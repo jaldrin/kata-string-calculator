3353c1731033e52a3d4bdb13388e6e48f9304e88
John Aldrin <john.aldrin@outlook.com>
2021-05-31 11:05:16 -0700

    Simple String conversions to: Lower, Upper, Title

--- StringDemo/Program.cs
+++ StringDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace StringDemo
 {
@@ -6,7 +7,28 @@ namespace StringDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StringConversion();
         }
+
+        private static void StringConversion()
+        {
+            string testString = "tHis iS tHe FBI Calling!";
+            TextInfo currentTextInfo = CultureInfo.CurrentCulture.TextInfo;
+            TextInfo englishTextInfo = new CultureInfo("en-US", false).TextInfo;
+            string result;
+
+            result = testString.ToLower();
+            Console.WriteLine(result);
+
+            result = testString.ToUpper();
+            Console.WriteLine(result);
+
+            result = currentTextInfo.ToTitleCase(testString);
+            Console.WriteLine(result);
+
+            result = englishTextInfo.ToTitleCase(testString);
+            Console.WriteLine(result);
+        }
+
     }
 }
