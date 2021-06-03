6a90dfc539a6d40aee2e01b859df64f6942a408f
John Aldrin <john.aldrin@outlook.com>
2021-06-03 16:55:16 -0700

    Add Customer Builder class

---
+++ BuilderTestSample/Tests/TestBuilder/CustomerBuilder.cs
@@ -0,0 +1,77 @@
+﻿using BuilderTestSample.Model;
+
+namespace BuilderTestSample.Tests.TestBuilder
+{
+    /// <summary>
+    /// Reference: https://ardalis.com/improve-tests-with-the-builder-pattern-for-test-data
+    /// </summary>
+    public class CustomerBuilder
+    {
+        private int _id;
+        private string _firstName;
+        private string _lastName;
+        private Address _homeAddress;
+        private int _creditRating;
+        private decimal _totalPurchases;
+
+        public CustomerBuilder Id(int id)
+        {
+            _id = id;
+            return this;
+        }
+
+        public CustomerBuilder FirstName(string firstName)
+        {
+            _firstName = firstName;
+            return this;
+        }
+
+        public CustomerBuilder LastName(string lastName)
+        {
+            _lastName = lastName;
+            return this;
+        }
+
+        public CustomerBuilder HomeAddress(Address homeAddress)
+        {
+            _homeAddress = homeAddress;
+            return this;
+        }
+
+        public CustomerBuilder CreditRating(int creditRating)
+        {
+            _creditRating = creditRating;
+            return this;
+        }
+
+        public CustomerBuilder TotalPurchases(decimal totalPurchases)
+        {
+            _totalPurchases = totalPurchases;
+            return this;
+        }
+
+        public Customer Build()
+        {
+            return new Customer(_id)
+            {
+                FirstName = _firstName,
+                LastName = _lastName,
+                HomeAddress = _homeAddress,
+                CreditRating = _creditRating,
+                TotalPurchases = _totalPurchases
+            };
+        }
+
+        public CustomerBuilder WithTestValues(int id)
+        {
+            _id = id;
+            _firstName = nameof(Customer.FirstName);
+            _lastName = nameof(Customer.LastName);
+            _homeAddress = null;
+            _creditRating = 600;
+            _totalPurchases = 100m;
+
+            return this;
+        }
+    }
+}
